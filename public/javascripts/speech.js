b28b4dfa55f9a2e72d9c9e398cfa870c6915da19
anavianm <anavianm@bxscience.edu>
2019-12-07 23:56:49 -0500

    Speech updates

--- public/javascripts/speech.js
+++ public/javascripts/speech.js
@@ -29,6 +29,8 @@ document.addEventListener("DOMContentLoaded", () => {
         // Render display 
         display(data);
     })
+    
+    
 })
 
 function display(data) {
@@ -62,40 +64,65 @@ function display(data) {
     document.getElementById("stepsToSpeech").setAttribute('data-words', stepsToSpeech);
     
     var currentList = document.getElementById('liked').innerHTML;
-console.log(currentList);
+    console.log(currentList);
 
-var recipes = currentList.split(",");
-var currentrecipe = loadedRecipe;
-//var currentrecipe = document.getElementById('recipeName').innerHTML;
-//console.log(ocument.getElementById('recipeName').innerHTML);
-console.log("curr is" + currentrecipe);
+    var recipes = currentList.split(",");
+    var currentrecipe = loadedRecipe;
+    //var currentrecipe = document.getElementById('recipeName').innerHTML;
+    //console.log(ocument.getElementById('recipeName').innerHTML);
+    console.log("curr is" + currentrecipe);
 
-//var update = current + recipe +",";
-var update = "Salmon Avocado Roll";
-document.getElementById('likedRecipe').value = update;
-
-console.log(document.getElementById("loggedIn").innerHTML);
-if((document.getElementById("loggedIn").innerHTML) == ""){
-    console.log("hello");
-     console.log("X sign in");
-    $("#signInPlease").show();
-    $("#heartButton").hide();
-}else{
-    console.log(recipes);
+    //var update = current + recipe +",";
+    var update = "Salmon Avocado Roll, Tuna Roll, Veggie Roll";
+                document.getElementById('likedRecipe').value = update;
     
-    if(recipes.includes(currentrecipe)){
-        console.log("This is a " + currentrecipe);
-        completedText = '<i class="fa fa-heart"></i>' + " Completed";
-        document.getElementById('heartButton').style.cssText = 'background-color:#000000; color:#ffffff;';
-        document.getElementById("heartButton").innerHTML = completedText;
-    }else{
-        console.log("sign in");
-        $("#heartButton").show();
-        $("#signInPlease").hide();
+    if((document.getElementById("loggedIn").innerHTML) == ""){
+        console.log("hello");
+         console.log("X sign in");
+        $("#signInPlease").show();
+        $("#heartButton").hide();
     }
-}
+    
+
+    console.log(document.getElementById("loggedIn").innerHTML);
+    
+//    if((document.getElementById("loggedIn").innerHTML) == ""){
+//        console.log("hello");
+//         console.log("X sign in");
+//        $("#signInPlease").show();
+//        $("#heartButton").hide();
+    if(document.getElementById("loggedIn").innerHTML != ""){
+        console.log(recipes);
+        console.log("this is inner" + document.getElementById('heartButton').innerHTML);
+        
+        if((document.getElementById('heartButton').innerHTML == '<i class="fa fa-heart"></i> Completed') && (recipes.includes(currentrecipe))){
+            for(var i = 0; i < recipes.length; i ++){
+                if(recipes[i] == currentrecipe){
+                    recipes.splice(i,1);
+                    console.log(recipes);
+                    currentList = recipes.join();
+                    document.getElementById('likedRecipe').value = currentList;
+                    completedText = '<i class="fa fa-heart"></i>' + " Complete";
+                    document.getElementById('heartButton').style.cssText = 'background-color:#ffffff; color:#000000;';
+                    document.getElementById("heartButton").innerHTML = completedText;
+                }
+            }
+            
+        }else if(recipes.includes(currentrecipe)){
+            document.getElementById('likedRecipe').value = update;
+            console.log("This is a " + currentrecipe);
+            completedText = '<i class="fa fa-heart"></i>' + " Completed";
+            document.getElementById('heartButton').style.cssText = 'background-color:#000000; color:#ffffff;';
+            document.getElementById("heartButton").innerHTML = completedText;
+        }else{
+            console.log("sign in");
+            $("#heartButton").show();
+            $("#signInPlease").hide();
+        }
+    }
 
 }
+    
 
 function speak(text) {
     // Built-in Web Speech API
@@ -105,45 +132,7 @@ function speak(text) {
     utterance.rate = 0.75;
     speechSynthesis.speak(utterance);
 }
-//
-//function complete(e) {
-//    if (!completed)  {
-//        localStorage.setItem(recipe, false);
-//        completed = 1;
-//        completedText = '<i class="fa fa-heart"></i>' + " Completed";
-//        document.getElementById('heartButton').style.cssText = 'background-color:#000000; color:#ffffff;';
-//        console.log(completed);
-//    } else {
-//        localStorage.setItem(recipe, true);
-//        console.log(localStorage(recipe));
-//        completed = 0;
-//        completedText = '<i class="fa fa-heart"></i>' + " Complete";
-//        document.getElementById('heartButton').style.cssText = 'background-color:#ffffff; color:#000000;';
-//        console.log(completed);
-//    } 
-//    document.getElementById("heartButton").innerHTML = completedText;
-//}
 
-
-$(document).ready(function() {
-
-
-});
-
-
-
-
-
-
-    
-//}else{
-//    //button open for click
-//    if(){//button is clicked
-//        //set button to clicked
-//    }else{
-//        //do nothing
-//    }
-//}
 
 
 function click(e) {
